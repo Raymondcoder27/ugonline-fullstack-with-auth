4cdda490d9b75dcfe49bff8e4ba8b9f9eba76959
RaymondCoder27 <mwebraymond13@gmail.com>
2025-02-14 12:42:08 +0300

    adds transaction id

--- backend/models/device.go
+++ backend/models/device.go
@@ -7,7 +7,8 @@ import (
 // Device represents a device associated with a user's session
 type Device struct {
 	// ID             string    `gorm:"primaryKey;type:uuid;default:uuid_generate_v4()" json:"id"`
-	ID             string    `gorm:"primaryKey; json:"id"`
+	// ID             string    `gorm:"primaryKey; json:"id"`
+	ID             string    `json:"id" gorm:"primaryKey"`
 	SessionID      string    `gorm:"not null" json:"session_id"`
 	IPAddress      string    `json:"ipAddress"`
 	UserAgent      string    `json:"userAgent"`
