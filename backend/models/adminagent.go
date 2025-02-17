fcf62dbcee5cb7ce35ea6293f2b528e23e503ee0
RaymondCoder27 <mwebraymond13@gmail.com>
2025-02-17 10:37:44 +0300

    adds transaction id

--- backend/models/adminagent.go
+++ backend/models/adminagent.go
@@ -4,13 +4,14 @@ import "time"
 
 // BranchManager represents a manager assigned to a specific branch.
 type BranchManager struct {
-	ID string `json:"id" gorm:"primaryKey"`
-	// AgentAdminAccountID ID     `json:"agentAdminAccountId" gorm:"foreignKey:AgentAdminAccountID"`
-	Username string `json:"username" gorm:"unique;not null"`
-	FullName string `json:"fullName" gorm:"not null"`
-	Role     string `json:"role" gorm:"not null"`   // e.g., "Manager"
-	Branch   string `json:"branch" gorm:"not null"` // e.g., "Branch 1"
-	Status   string `json:"status" gorm:"not null"` // e.g., "Active", "Inactive"
+	ID                  string            `json:"id" gorm:"primaryKey"`
+	AgentAdminAccountID string            `json:"agentAdminAccountId"`
+	AgentAdminAccount   AgentAdminAccount `json:"agentAdminAccountId" gorm:"foreignKey:AgentAdminAccountID"`
+	Username            string            `json:"username" gorm:"unique;not null"`
+	FullName            string            `json:"fullName" gorm:"not null"`
+	Role                string            `json:"role" gorm:"not null"`   // e.g., "Manager"
+	Branch              string            `json:"branch" gorm:"not null"` // e.g., "Branch 1"
+	Status              string            `json:"status" gorm:"not null"` // e.g., "Active", "Inactive"
 }
 
 type AgentAdminFloatLedger struct {
