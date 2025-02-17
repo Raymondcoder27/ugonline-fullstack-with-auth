58e3d0c6336cbfe5ef5c272c1c4605f5725ab6ce
RaymondCoder27 <mwebraymond13@gmail.com>
2025-02-17 10:24:30 +0300

    adds transaction id

--- backend/models/adminagent.go
+++ backend/models/adminagent.go
@@ -4,13 +4,13 @@ import "time"
 
 // BranchManager represents a manager assigned to a specific branch.
 type BranchManager struct {
-	ID                string            `json:"id" gorm:"primaryKey"`
-	AgentAdminAccount AgentAdminAccount `json:"agentAdminAccount" gorm:"foreignKey:AgentAdminAccountID"`
-	Username          string            `json:"username" gorm:"unique;not null"`
-	FullName          string            `json:"fullName" gorm:"not null"`
-	Role              string            `json:"role" gorm:"not null"`   // e.g., "Manager"
-	Branch            string            `json:"branch" gorm:"not null"` // e.g., "Branch 1"
-	Status            string            `json:"status" gorm:"not null"` // e.g., "Active", "Inactive"
+	ID                  string `json:"id" gorm:"primaryKey"`
+	AgentAdminAccountID ID     `json:"agentAdminAccountId" gorm:"foreignKey:AgentAdminAccountID"`
+	Username            string `json:"username" gorm:"unique;not null"`
+	FullName            string `json:"fullName" gorm:"not null"`
+	Role                string `json:"role" gorm:"not null"`   // e.g., "Manager"
+	Branch              string `json:"branch" gorm:"not null"` // e.g., "Branch 1"
+	Status              string `json:"status" gorm:"not null"` // e.g., "Active", "Inactive"
 }
 
 type AgentAdminFloatLedger struct {
@@ -22,12 +22,3 @@ type AgentAdminFloatLedger struct {
 	CreatedAt   time.Time `json:"createdAt" gorm:"autoCreateTime"`
 	UpdatedAt   time.Time `json:"updatedAt" gorm:"autoUpdateTime"`
 }
-
-// FloatAllocation represents the allocation of float to branches.
-// type FloatAllocation struct {
-// 	ID           string      `json:"id" gorm:"primaryKey"`
-// 	DateAssigned time.Time `json:"dateAssigned" gorm:"not null"`
-// 	Amount       float64   `json:"amount" gorm:"not null"`
-// 	Status       string    `json:"status" gorm:"not null"` // e.g., "Allocated", "Pending", "Failed"
-// 	Branch       string    `json:"branch" gorm:"not null"` // e.g., "Branch 1"
-// }
