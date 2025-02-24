10d13bd482d6d41acb1c3d74a9f107b383cac6c5
RaymondCoder27 <mwebraymond13@gmail.com>
2025-02-24 18:12:37 +0300

    adds file with role based navigation guardst

--- frontend/src/branchmanagerdomain/finances/Transactions.vue
+++ frontend/src/branchmanagerdomain/finances/Transactions.vue
@@ -90,6 +90,17 @@ const updateFilter = useDebounceFn(() => fetch(), 300, { maxWait: 5000 });
 // Date conversion utility
 // const convertDate = (date: string) => moment(date).format("DD-MM-YYYY");
 
+
+const selectedTransaction: Ref<Transaction | undefined> = ref();
+
+// const modalOpen: Ref<boolean> = ref(false);
+
+function view(transaction: Transaction) {
+  modalOpen.value = true;
+  selectedTransaction.value = transaction;
+}
+
+
 function convertDateTime(date: string) {
   return moment(date).format("DD-MM-YYYY HH:mm:ss");
 }
@@ -290,9 +301,10 @@ onMounted(() => {
               <label class="cursor-pointer hover:text-primary-700 mx-2">
                 <span class="hover:underline">{{ transaction.id }}</span>
               </label>
+              
             </td>
             <!-- <td>{{ idx + 1 }}</td> -->
-            <td class="rounded-md font-semibold text-red-700">
+            <!-- <td class="rounded-md font-semibold text-red-700">
               <span
                 class="hover:underline"
                 @click="transactionDetails(transaction.id)"
@@ -302,7 +314,21 @@ onMounted(() => {
                 @click="copyToClipboard(transaction.trackingNumber)"
                 class="fa-regular fa-copy mx-1 hover:text-gray-800"
               ></i>
-            </td>
+            </td> -->
+            <td class="rounded-md font-semibold text-red-700">
+                <!-- <span
+                  @click="transactionDetails(transaction.id)"
+                  class="hover:underline"
+                > -->
+                <span @click="view(transaction)" class="hover:underline">
+                  {{ transaction.trackingNumber }}
+                </span>
+                <!-- make it copy to clipboard -->
+                <i
+                  @click="copyToClipboard(transaction.trackingNumber)"
+                  class="fa-regular fa-copy mx-1 ml-4 hover:text-gray-800"
+                ></i>
+              </td>
             <td class="rounded-md font-semibold text-red-700 hover:underline">
               <span
                 class="hover:underline"
@@ -454,6 +480,98 @@ onMounted(() => {
       </div>
     </div>
   </div>
+
+
+    <!-- Modal -->
+    <AppModal v-model="modalOpen" xl4>
+      <p class="text-xl font-bold">Transaction Detail</p>
+      <div class="flex">
+        <div class="w-full">
+          <table class="w-12">
+            <thead>
+              <tr>
+                <th class="w-6/12">Field</th>
+                <th>Data</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr class="border border-gray-50">
+                <td class="p-1 font-bold">ID</td>
+                <td class="p-1">{{ selectedTransaction?.id }}</td>
+              </tr>
+              <!-- <tr class="border border-gray-50">
+              <td class="p-1 font-bold">Tracking Number</td>
+              <td class="p-1">{{ selectedTransaction?.trackingNo }}</td>
+            </tr> -->
+              <!-- <tr class="border border-gray-50">
+              <td class="p-1 font-bold">Date</td>
+              <td class="p-1">{{ selectedTransaction?.dateRequested }}</td>
+            </tr> -->
+              <tr class="border border-gray-50">
+                <td class="p-1 font-bold">Service</td>
+                <td class="p-1">{{ selectedTransaction?.service }}</td>
+              </tr>
+              <tr class="border border-gray-50">
+                <td class="p-1 font-bold">Provider</td>
+                <td class="p-1">{{ selectedTransaction?.provider }}</td>
+              </tr>
+              <tr class="border border-gray-50">
+                <td class="p-1 font-bold">Fee</td>
+                <td class="p-1">{{ selectedTransaction?.fee }}</td>
+              </tr>
+              <tr class="border border-gray-50">
+                <td class="p-1 font-bold">Status</td>
+                <td class="p-1">
+                  <div class="flex">
+                    <div class="w-6/12">
+                      <div :class="statusStyling(selectedTransaction?.status)">
+                        <div class="w-4/12 text-center">
+                          <i
+                            :class="statusIcon(selectedTransaction?.status)"
+                          ></i>
+                        </div>
+                        <div class="w-8/12">
+                          <label v-if="selectedTransaction?.status == 'SENT'">
+                            PROCESSING
+                          </label>
+                          <label v-else>
+                            {{ selectedTransaction?.status.replace("_", " ") }}
+                          </label>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </td>
+              </tr>
+              <!-- <tr class="border border-gray-50">
+              <td class="p-1 font-bold">Service</td>
+              <td class="p-1">{{ selectedTransaction?.serviceName }}</td>
+            </tr>
+            <tr class="border border-gray-50">
+              <td class="p-1 font-bold">Provider</td>
+              <td class="p-1">{{ selectedTransaction?.providerName }}</td>
+            </tr> -->
+              <tr class="border border-gray-50">
+                <td class="p-1 font-bold">Date</td>
+                <td class="p-1">
+                  {{ convertDateTimeNullable(selectedTransaction?.createdAt) }}
+                </td>
+              </tr>
+              <!-- <tr class="border border-gray-50">
+              <td class="p-1 font-bold">Payload</td>
+              <td class="p-1 bg-gray-50">
+                <div class="flex">
+                  <div class="w-full  rounded">
+                    <pre>{{ selectedTransaction?.data }}</pre>
+                  </div>
+                </div>
+              </td>
+            </tr> -->
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </AppModal>
 
   <!-- Modal -->
   <AppModal v-model="modalOpen" xl2>
